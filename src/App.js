16918fbbc1e3a8d26183a524a099d9af483675bd
Ivan Garcia <ivangm180@gmail.com>
2021-10-29 10:36:31 -0500

    creating the todo list skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./App.css";
 
+
+// Components
+import {TodoCounter} from './components/TodoCounter'
+import { CreateTodoButton } from "./components/CreateTodoButton";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { TodoSearch } from "./components/TodoSearch";
+
+
 const todos = [
   { text: "Ir al super", completed: false },
   { text: "Llorar con la llorona", completed: false },
@@ -11,17 +20,14 @@ const todos = [
 function App() {
   return (
     <>
-     {/* { <TodoCounter />} */}
-      <h2>Hola como andamos?</h2>
-      {/* <TodoSearch /> */}
-      <input placeholder="Onions" />
-      {/* {<TodoList>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
         {todos.map((e) => (
-          <TodoItem />
-        ))}} */}
-      {/* {</TodoList>} */}
-      {/* {<CreateTodoButton />} */}
-      <button>+</button>
+          <TodoItem text={e.text}/>
+        ))}
+      </TodoList>
+      {<CreateTodoButton />}
     </>
   );
 }
