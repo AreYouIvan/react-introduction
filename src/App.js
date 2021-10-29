d499b9af06fadb56dd8ab6313d805f06a174413d
Ivan Garcia <ivangm180@gmail.com>
2021-10-29 10:02:25 -0500

    building the first part of the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const todos = [
+  { text: "Ir al super", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+  { text: "Hacer ejercicio", completed: false },
+  { text: "Leer mi libro de Maquiavelo", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     {/* { <TodoCounter />} */}
+      <h2>Hola como andamos?</h2>
+      {/* <TodoSearch /> */}
+      <input placeholder="Onions" />
+      {/* {<TodoList>
+        {todos.map((e) => (
+          <TodoItem />
+        ))}} */}
+      {/* {</TodoList>} */}
+      {/* {<CreateTodoButton />} */}
+      <button>+</button>
+    </>
   );
 }
 
