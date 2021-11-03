248602d884200c1dcb6d6a22cac0c559734434d1
Ivan Garcia <ivangm180@gmail.com>
2021-11-03 07:53:18 -0600

    knowing the state of react

--- src/components/TodoSearch.js
+++ src/components/TodoSearch.js
@@ -3,21 +3,28 @@ import React from "react";
 import "./styleComponents/TodoSearch.css";
 
 function TodoSearch() {
-  const onSearchValueChange = (event) => {
-    console.log(event.target.value)
-  }
+  const [searchValue, setSearchValue] = React.useState();
 
-  return (
+
+  const onSearchValueChange = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  return [
     <div className="TodoSearch-content">
       <input
         onChange={onSearchValueChange}
         className="TodoSearch-input"
         type="text"
         placeholder="Search.."
+        value={searchValue}
       />
       <span className="TodoSearch-input-icon"></span>
-    </div>
-  );
+    </div>,
+    <p>{searchValue}</p>,
+  ];
+
 }
 
 export { TodoSearch };
