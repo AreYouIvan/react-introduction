02c1cc81f2bb3cc0d54f7b2a6ec1c3cc5a645f39
Ivan Garcia <ivangm180@gmail.com>
2021-11-20 08:11:22 -0600

    adding the third form to make the validation

--- src/components/CreateTodoButton/index.js
+++ src/components/CreateTodoButton/index.js
@@ -8,19 +8,26 @@ There are two ways to make a toggle
 functions for active the modal.
 */
   // This is the first one.
-  
+
   // React.useContext(TodoContext);
+  // const onClickButton = () => {
+  // props.setOpenModal(!props.openModal);
+  // };
 
+  // const onClickButton = () => {
+  //   if (!props.openModal) {
+  //     props.setOpenModal(true);
+  //   } else {
+  //     props.setOpenModal(false);
+  //   }
+  // };
+
+// 3rd:
   const onClickButton = () => {
-      // props.setOpenModal(!props.openModal);
-      if (!props.openModal) {
-        props.setOpenModal(true);
-      } else {
-        props.setOpenModal(false);
-      }
-    };
+    props.setOpenModal((prevState) => !prevState);
+  };
 
-    // This is the second.
+  // This is the second.
 
   // };
 
